a0ff5570ff6eb6356684a81cf174d68d4949da33
Fraser Molyneux <frasermolyneux@mx-mail.io>
2023-01-15 18:07:01 +0000

    Fix naming

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -3,6 +3,6 @@ locals {
   key_vault_name            = "kv-${random_id.environment_id.hex}-${var.location}"
   app_insights_name         = "ai-ptl-event-ingest-${random_id.environment_id.hex}-${var.environment}-${var.location}"
   function_app_name         = "fa-ptl-event-ingest-${random_id.environment_id.hex}-${var.environment}-${var.location}"
-  function_app_storage_name = "saptlevntinfn${random_id.environment_id.hex}"
+  function_app_storage_name = "saptlevin${random_id.environment_id.hex}"
   service_bus_name          = "sb-ptl-event-ingest-${random_id.environment_id.hex}-${var.environment}-${var.location}"
 }
