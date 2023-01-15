329cf59986f152e4010936732f7810f5faab459c
Fraser Molyneux <frasermolyneux@mx-mail.io>
2023-01-15 17:33:05 +0000

    Fixing duplicate resource names

--- terraform/service_bus.tf
+++ terraform/service_bus.tf
@@ -14,22 +14,22 @@ resource "azurerm_servicebus_queue" "player_connected" {
 }
 
 
-resource "azurerm_servicebus_queue" "player_connected" {
+resource "azurerm_servicebus_queue" "chat_message" {
   name         = "chat_message_queue"
   namespace_id = azurerm_servicebus_namespace.ingest.id
 }
 
-resource "azurerm_servicebus_queue" "player_connected" {
+resource "azurerm_servicebus_queue" "map_vote" {
   name         = "map_vote_queue"
   namespace_id = azurerm_servicebus_namespace.ingest.id
 }
 
-resource "azurerm_servicebus_queue" "player_connected" {
+resource "azurerm_servicebus_queue" "server_connected" {
   name         = "server_connected_queue"
   namespace_id = azurerm_servicebus_namespace.ingest.id
 }
 
-resource "azurerm_servicebus_queue" "player_connected" {
+resource "azurerm_servicebus_queue" "map_change" {
   name         = "map_change_queue"
   namespace_id = azurerm_servicebus_namespace.ingest.id
 }
