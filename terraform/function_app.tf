5ef812a302646664320a1fe0f4efd5ddd01253fc
Fraser Molyneux <frasermolyneux@mx-mail.io>
2023-10-27 22:22:17 +0100

    Updating portal-repository

--- terraform/function_app.tf
+++ terraform/function_app.tf
@@ -33,14 +33,15 @@ resource "azurerm_linux_function_app" "app" {
   }
 
   app_settings = {
-    "READ_ONLY_MODE"                             = var.environment == "prd" ? "true" : "false"
-    "WEBSITE_RUN_FROM_PACKAGE"                   = "1"
-    "ApplicationInsightsAgent_EXTENSION_VERSION" = "~3"
-    "service_bus_connection_string"              = format("@Microsoft.KeyVault(VaultName=%s;SecretName=%s)", azurerm_key_vault.kv.name, azurerm_key_vault_secret.service_bus_connection_string_secret.name)
-    "apim_base_url"                              = data.azurerm_api_management.platform.gateway_url
-    "portal_repository_apim_subscription_key"    = format("@Microsoft.KeyVault(VaultName=%s;SecretName=%s)", azurerm_key_vault.kv.name, azurerm_key_vault_secret.repository_api_subscription_secret.name)
-    "repository_api_application_audience"        = var.repository_api.application_audience
-    "repository_api_path_prefix"                 = var.repository_api.apim_path_prefix
+    "READ_ONLY_MODE"                                    = var.environment == "prd" ? "true" : "false"
+    "WEBSITE_RUN_FROM_PACKAGE"                          = "1"
+    "ApplicationInsightsAgent_EXTENSION_VERSION"        = "~3"
+    "service_bus_connection_string"                     = format("@Microsoft.KeyVault(VaultName=%s;SecretName=%s)", azurerm_key_vault.kv.name, azurerm_key_vault_secret.service_bus_connection_string_secret.name)
+    "apim_base_url"                                     = data.azurerm_api_management.platform.gateway_url
+    "portal_repository_apim_subscription_key_primary"   = format("@Microsoft.KeyVault(VaultName=%s;SecretName=%s)", azurerm_key_vault.kv.name, azurerm_key_vault_secret.repository_api_subscription_secret_primary.name)
+    "portal_repository_apim_subscription_key_secondary" = format("@Microsoft.KeyVault(VaultName=%s;SecretName=%s)", azurerm_key_vault.kv.name, azurerm_key_vault_secret.repository_api_subscription_secret_secondary.name)
+    "repository_api_application_audience"               = var.repository_api.application_audience
+    "repository_api_path_prefix"                        = var.repository_api.apim_path_prefix
   }
 }
 
