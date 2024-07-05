bf6f51b89a166cf0927bd207aaf6155739309453
Fraser Molyneux <frasermolyneux@mx-mail.io>
2024-07-05 23:18:41 +0100

    Add legacy provider back in

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -12,6 +12,10 @@ variable "instance" {
 
 variable "subscription_id" {}
 
+variable "legacy_api_management_subscription_id" {}
+variable "legacy_api_management_resource_group_name" {}
+variable "legacy_api_management_name" {}
+
 variable "api_management_name" {}
 
 variable "repository_api" {
