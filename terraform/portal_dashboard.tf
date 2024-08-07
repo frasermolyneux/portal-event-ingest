f22661b48562f8344f33da403fa825f1594c80fc
Fraser Molyneux <frasermolyneux@mx-mail.io>
2024-08-07 19:54:21 +0100

    Create dashboard

---
+++ terraform/portal_dashboard.tf
@@ -0,0 +1,10 @@
+resource "azurerm_portal_dashboard" "app" {
+  name = local.dashboard_name
+
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+
+  tags = var.tags
+
+  dashboard_properties = readfile("dashboards/dashboard.json")
+}
