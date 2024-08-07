35a6e6809579895b140cd96caac63b72aec66559
Fraser Molyneux <frasermolyneux@mx-mail.io>
2024-08-07 20:08:30 +0100

    Add output

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,7 @@
 output "function_app_name" {
   value = azurerm_linux_function_app.app.name
 }
+
+output "dashboard_properties" {
+  value = data.azurerm_portal_dashboard.app.dashboard_properties
+}
