7bd23da8ddc40af77916395dc1b328e3d4a1ec53
Fraser Molyneux <frasermolyneux@mx-mail.io>
2024-09-19 18:46:32 +0100

    Adding flex app service plan

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -3,6 +3,7 @@ locals {
   key_vault_name            = "kv-${random_id.environment_id.hex}-${var.location}"
   function_app_name         = "fn-portal-event-ingest-${var.environment}-${var.location}-${var.instance}-${random_id.environment_id.hex}"
   function_app_storage_name = "safn${random_id.environment_id.hex}"
+  app_service_plan_name     = "asp-portal-event-ingest-${var.environment}-${var.location}-${var.instance}"
   service_bus_name          = "sb-portal-event-ingest-${var.environment}-${var.location}-${var.instance}-${random_id.environment_id.hex}"
   app_registration_name     = "portal-event-ingest-${var.environment}-${var.instance}"
   dashboard_name            = "portal-event-ingest-${var.environment}-${var.instance}"
