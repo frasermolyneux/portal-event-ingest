8b8b428cc6c199ebd6223cdbf1ec00106ac7770c
Fraser Molyneux <frasermolyneux@mx-mail.io>
2025-06-28 17:14:34 +0100

    Refactor API management configuration: replace apim_api_name and apim_api_revision with apim_product_id, update related variables and remove unused data source.

--- terraform/api_subscriptions.tf
+++ terraform/api_subscriptions.tf
@@ -5,6 +5,6 @@ resource "azurerm_api_management_subscription" "repository_api_subscription" {
   state         = "active"
   allow_tracing = false
 
-  api_id       = split(";", data.azurerm_api_management_api.repository_api.id)[0] // Strip revision from id when creating subscription
+  product_id   = data.azurerm_api_management_product.repository_api_product.id
   display_name = format("%s-%s", local.function_app_name, data.azurerm_api_management_api.repository_api.name)
 }
