94f411781118e6035b374dbbc3b413a195f5180f
Fraser Molyneux <frasermolyneux@mx-mail.io>
2025-10-31 22:13:02 +0000

    Refactor OnChatMessage alert to use scheduled query rules and update query criteria

--- terraform/monitor_metric_alerts.tf
+++ terraform/monitor_metric_alerts.tf
@@ -1,36 +1,38 @@
-resource "azurerm_monitor_metric_alert" "on_chat_message_missing" {
+resource "azurerm_monitor_scheduled_query_rules_alert" "on_chat_message_missing" {
   count = var.environment == "prd" ? 1 : 0
 
-  name = "portal-event-ingest-${var.environment} - OnChatMessage custom event - missing"
-
+  name                = "portal-event-ingest-${var.environment} - OnChatMessage custom event - missing"
   resource_group_name = azurerm_resource_group.rg.name
-  scopes              = [data.azurerm_application_insights.core.id]
+  location            = azurerm_resource_group.rg.location
 
   description = "Triggers when the OnChatMessage custom event has not been ingested for six hours."
 
-  frequency   = "PT1H"
-  window_size = "PT6H"
-
-  criteria {
-    metric_namespace = "microsoft.insights/components"
-    metric_name      = "customEvents"
-    aggregation      = "Count"
-    operator         = "LessThan"
-    threshold        = 1
-
-    dimension {
-      name     = "name"
-      operator = "Include"
-      values   = ["OnChatMessage"]
-    }
-
-    skip_metric_validation = false
-  }
+  frequency   = 60
+  time_window = 360
 
   severity = 0
+  enabled  = true
+
+  data_source_id          = data.azurerm_application_insights.core.id
+  authorized_resource_ids = [data.azurerm_application_insights.core.id]
+
+  query = <<-QUERY
+    customEvents
+    | where name == "OnChatMessage"
+    | where timestamp >= ago(6h)
+    | summarize Events = count()
+    | project Events
+  QUERY
+
+  trigger {
+    operator  = "LessThan"
+    threshold = 1
+  }
 
   action {
-    action_group_id = data.azurerm_monitor_action_group.critical.id
+    action_group = [
+      data.azurerm_monitor_action_group.critical.id,
+    ]
   }
 
   tags = var.tags
