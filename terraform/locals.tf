cfb809728f30ca659df32ff9b704eec8c51473c1
Fraser Molyneux <frasermolyneux@mx-mail.io>
2026-01-03 03:16:40 +0000

    Add remote state configurations and update variable definitions for platform workloads, monitoring, and portal environments

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -1,4 +1,21 @@
 locals {
+  workload_resource_groups = {
+    for location in [var.location] :
+    location => data.terraform_remote_state.platform_workloads.outputs.workload_resource_groups[var.workload_name][var.environment].resource_groups[lower(location)]
+  }
+
+  workload_backend = try(
+    data.terraform_remote_state.platform_workloads.outputs.workload_terraform_backends[var.workload_name][var.environment],
+    null
+  )
+
+  workload_administrative_unit = try(
+    data.terraform_remote_state.platform_workloads.outputs.workload_administrative_units[var.workload_name][var.environment],
+    null
+  )
+
+  workload_resource_group = local.workload_resource_groups[var.location]
+
   resource_group_name       = "rg-portal-event-ingest-${var.environment}-${var.location}-${var.instance}"
   key_vault_name            = "kv-${random_id.environment_id.hex}-${var.location}"
   function_app_name         = "fn-portal-event-ingest-${var.environment}-${var.location}-${var.instance}-${random_id.environment_id.hex}"
