6ff2c07f4749f65dc892046436a2193cf24f0aa9
Fraser Molyneux <frasermolyneux@mx-mail.io>
2026-01-03 18:50:30 +0000

    Update key vault and service bus naming conventions for consistency; adjust API management name to reflect the correct location.

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -19,11 +19,11 @@ locals {
 
   # Local Resource Naming
   resource_group_name       = "rg-portal-event-ingest-${var.environment}-${var.location}-${var.instance}"
-  key_vault_name            = "kv-${random_id.environment_id.hex}-${var.location}"
+  key_vault_name            = substr(format("kv-%s-%s", random_id.environment_id.hex, var.location), 0, 24)
   function_app_name         = "fn-portal-event-ingest-${var.environment}-${var.location}-${var.instance}-${random_id.environment_id.hex}"
   function_app_storage_name = "safn${random_id.environment_id.hex}"
   app_service_plan_name     = "asp-portal-event-ingest-${var.environment}-${var.location}-${var.instance}"
-  service_bus_name          = "sb-portal-event-ingest-${var.environment}-${var.location}-${var.instance}-${random_id.environment_id.hex}"
+  service_bus_name          = substr(format("sb-portal-event-ingest-%s-%s-%s-%s", var.environment, var.location, var.instance, random_id.environment_id.hex), 0, 50)
   app_registration_name     = "portal-event-ingest-${var.environment}-${var.instance}"
   dashboard_name            = "portal-event-ingest-${var.environment}-${var.instance}"
 }
