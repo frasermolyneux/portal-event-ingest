d5f6ddaf85a0f674e9f8093785f82ebeb5356d8e
Fraser Molyneux <frasermolyneux@mx-mail.io>
2026-01-05 21:19:25 +0000

    Refactor legacy API subscription to use local variables for API Management configuration

--- terraform/legacy_api_subscriptions.tf
+++ terraform/legacy_api_subscriptions.tf
@@ -1,10 +1,10 @@
 resource "azurerm_api_management_subscription" "legacy_repository_api_subscription" {
-  api_management_name = data.azurerm_api_management.core.name
-  resource_group_name = data.azurerm_api_management.core.resource_group_name
+  api_management_name = local.core_api_management.name
+  resource_group_name = local.core_api_management.resource_group_name
 
   state         = "active"
   allow_tracing = false
 
-  product_id   = data.azurerm_api_management_product.repository_api_product.id
-  display_name = format("%s-%s", local.legacy_function_app_name, data.azurerm_api_management_product.repository_api_product.product_id)
+  product_id   = local.event_ingest_api_shared.product_id
+  display_name = format("%s-%s", local.legacy_function_app_name, local.event_ingest_api_shared.product_id)
 }
