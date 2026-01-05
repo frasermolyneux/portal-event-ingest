2f4a5ca7dbbc30bd6965402398d6ec2cea84dbcf
Fraser Molyneux <frasermolyneux@mx-mail.io>
2026-01-05 19:53:37 +0000

    Update API policy audience and remove legacy application registration

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -22,6 +22,8 @@ locals {
   managed_identities            = try(data.terraform_remote_state.portal_environments.outputs.managed_identities, {})
   event_ingest_funcapp_identity = local.managed_identities["event_ingest_funcapp_identity"]
 
+  event_ingest_api_identifier_uri = data.terraform_remote_state.portal_environments.outputs.event_ingest_api.application.primary_identifier_uri
+
   # Local Resource Naming
   legacy_resource_group_name       = "rg-portal-event-ingest-${var.environment}-${var.location}-${var.instance}"
   legacy_key_vault_name            = substr(format("kv-%s-%s", random_id.legacy_environment_id.hex, var.location), 0, 24)
@@ -29,6 +31,5 @@ locals {
   legacy_function_app_storage_name = "safn${random_id.legacy_environment_id.hex}"
   legacy_app_service_plan_name     = "asp-portal-event-ingest-${var.environment}-${var.location}-${var.instance}"
   legacy_service_bus_name          = substr(format("sb-portal-event-ingest-%s-%s-%s-%s", var.environment, var.location, var.instance, random_id.legacy_environment_id.hex), 0, 50)
-  legacy_app_registration_name     = "portal-event-ingest-${var.environment}-${var.instance}"
   legacy_dashboard_name            = "portal-event-ingest-${var.environment}-${var.instance}"
 }
