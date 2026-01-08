9eff4219cf58d8a6532151ff7a3c39311c95cab7
Fraser Molyneux <frasermolyneux@mx-mail.io>
2026-01-08 21:39:54 +0000

    Remove unused SQL Server data configuration from Terraform

--- terraform/data.sql_server.tf
+++
@@ -1,4 +0,0 @@
-data "azurerm_sql_server" "sql_server" {
-  name                = local.workload_backend.sql_server.name
-  resource_group_name = local.workload_backend.sql_server.resource_group_name
-}
