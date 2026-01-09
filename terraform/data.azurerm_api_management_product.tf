43cb9dd4d14023324ab27fc64c6c3d0f2ab217d6
Fraser Molyneux <frasermolyneux@mx-mail.io>
2026-01-09 05:49:22 +0000

    Remove unused API Management product data resource

--- terraform/data.azurerm_api_management_product.tf
+++
@@ -1,6 +0,0 @@
-data "azurerm_api_management_product" "repository_api_product" {
-  product_id = local.repository_api.api_management.root_path
-
-  api_management_name = local.api_management.name
-  resource_group_name = local.api_management.resource_group_name
-}
