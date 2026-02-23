33fcdb424d0b794b8a317f67e1c0b115a6f6695a
Fraser Molyneux <34033625+frasermolyneux@users.noreply.github.com>
2026-02-23 23:00:36 +0000

    feat: integrate Azure App Configuration for enhanced settings management and update project dependencies (#456)

--- terraform/function_app.tf
+++ terraform/function_app.tf
@@ -77,9 +77,6 @@ resource "azurerm_linux_function_app" "function_app" {
     "ServiceBusConnection__fullyQualifiedNamespace" = format("%s.servicebus.windows.net", azurerm_servicebus_namespace.sb.name)
     "ServiceBusConnection__ManagedIdentityClientId" = local.event_ingest_identity.client_id
 
-    "RepositoryApi__BaseUrl"             = local.repository_api.api_management.endpoint
-    "RepositoryApi__ApplicationAudience" = local.repository_api.application.primary_identifier_uri
-
     // https://learn.microsoft.com/en-us/azure/azure-monitor/profiler/profiler-azure-functions#app-settings-for-enabling-profiler
     "APPINSIGHTS_PROFILERFEATURE_VERSION"  = "1.0.0"
     "DiagnosticServices_EXTENSION_VERSION" = "~3"
